4f57590430324791af77afdb9a9ebc1cec97c83c
Jaime Convery <jaimeconvery@gmail.com>
2019-08-23 13:43:32 -0400

    js files

--- js/date.js
+++ js/date.js
@@ -1,8 +1,3 @@
 // Generate the date to show in fields
 date = document.getElementById("date").valueAsDate = new Date();
 
-input = document.getElementsByClassName("myInput");
-
-for (var i = 0; i<input.length; i++) {
-	input[i].addEventListener('keyup', filter, false);
-} 
